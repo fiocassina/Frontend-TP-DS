fe3fd62f12f3c6b672f85f9d340a2ec06005fc63
Lucila Vega <lucilavega45@gmail.com>
2025-10-04 16:41:40 -0300

    Merge branch 'usuario-clases' of https://github.com/fiocassina/Frontend-TP-DS into usuario-clases

---
+++ cliente/.angular/cache/20.1.0/cliente/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,40 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-XDG7ZH3S.js";
+import "./chunk-XW55JBRW.js";
+import "./chunk-ZO3L2GH5.js";
+import "./chunk-7PDVBWPH.js";
+import "./chunk-C344PHYX.js";
+import "./chunk-F3OVQXGI.js";
+import "./chunk-TGGGLMIV.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
